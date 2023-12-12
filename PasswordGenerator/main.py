8806913d556079bc4e6cc82b366883e9cf99827e
peninahnaserian <peshnase@gmail.com>
2023-12-12 22:04:15 +0300

    Mini

---
+++ PasswordGenerator/main.py
@@ -0,0 +1,38 @@
+import random
+import string
+
+def generate_password(min_length,numbers=True,special_char=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+    
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_char:
+        characters += special
+        
+    pwd = ""
+    meet_criteria = False
+    has_number = False
+    has_special = False
+    
+    while not meet_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+        
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+        
+        meet_criteria = True
+        if numbers:
+            meet_criteria = has_number
+        if special_char:
+            meet_criteria = meet_criteria and has_special
+    
+    return pwd
+
+pwd = generate_password(10)
+print(pwd)
