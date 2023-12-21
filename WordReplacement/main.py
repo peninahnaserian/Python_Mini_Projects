753dfcfc166a33c76db595d3bbd79eabb6d82d13
peninahnaserian <peshnase@gmail.com>
2023-12-21 11:09:10 +0300

    Mini

---
+++ WordReplacement/main.py
@@ -0,0 +1,9 @@
+def replace_word():
+    
+    str = "Hola guys! I am Penny and hi hi hi"
+    word_to_replace = input("Enter the word to replace: ")
+    word = input("Enter the word replacement: ")
+    
+    print(str.replace(word_to_replace,word))
+    
+replace_word()
